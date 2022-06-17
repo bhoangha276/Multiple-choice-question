879339c27d71bcd7f937ae091ae562550bd1efcc
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2022-06-17 17:11:44 +0700

    view-detail-exam

---
+++ Views/exams/detail-exam.php
@@ -0,0 +1,125 @@
+<!-- inner page banner -->
+<div class="page-banner ovbl-dark" style="background-image:url(assets/images/banner/banner3.jpg);">
+    <div class="container">
+        <div class="page-banner-entry">
+            <h1 class="text-white">Exams</h1>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb row -->
+<div class="breadcrumb-row">
+    <div class="container">
+        <ul class="list-inline">
+            <li><a href="#">Home</a></li>
+            <li>Exams</li>
+            <li>Detail</li>
+        </ul>
+    </div>
+</div>
+<!-- Breadcrumb row END -->
+<!-- inner page banner END -->
+<div class="content-block">
+    <!-- About Us -->
+    <div class="section-area section-sp1 gallery-bx">
+        <div class="container">
+            <div class="container mb-5">
+                <h5 class="card-header">Exam</h5>
+                <div class="row mt-5">
+                    <div class="col-12">
+                        <p class="fw-bold">1. Which of the following sentences is correct</p>
+                        <div>
+                            <input type="radio" name="box" id="one" />
+                            <input type="radio" name="box" id="two" />
+                            <input type="radio" name="box" id="three" />
+                            <input type="radio" name="box" id="four" />
+                            <label for="one" class="box first">
+                                <div class="course">
+                                    <span class="circle"></span>
+                                    <span class="subject">
+                                        When its raining ,people's umbrella are all you're going to
+                                        see from above
+                                    </span>
+                                </div>
+                            </label>
+                            <label for="two" class="box second">
+                                <div class="course">
+                                    <span class="circle"></span>
+                                    <span class="subject">
+                                        When its raining,people's umbrella are all your going to see
+                                        from above
+                                    </span>
+                                </div>
+                            </label>
+                            <label for="three" class="box third">
+                                <div class="course">
+                                    <span class="circle"></span>
+                                    <span class="subject">
+                                        When its raining,peoples umbrella's are all you're going to
+                                        see from above
+                                    </span>
+                                </div>
+                            </label>
+                            <label for="four" class="box forth">
+                                <div class="course">
+                                    <span class="circle"></span>
+                                    <span class="subject"> None of the above </span>
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <p class="fw-bold mt-5">
+                            2. Complete the following sentences:Alice couldn't _______ the
+                            humilation any longer and stormed out of the room red as a bed
+                        </p>
+                        <div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <input type="radio" name="box" id="five" />
+                                    <label for="five" class="box fifth w-100">
+                                        <div class="course">
+                                            <span class="circle"></span> <span class="subject">is</span>
+                                        </div>
+                                    </label>
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="radio" name="box" id="six" />
+                                    <label for="six" class="box sixth w-100">
+                                        <div class="course">
+                                            <span class="circle"></span>
+                                            <span class="subject"> was </span>
+                                        </div>
+                                    </label>
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="radio" name="box" id="seven" />
+                                    <label for="seven" class="box seveth w-100">
+                                        <div class="course">
+                                            <span class="circle"></span>
+                                            <span class="subject"> will </span>
+                                        </div>
+                                    </label>
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="radio" name="box" id="eight" />
+                                    <label for="eight" class="box eighth w-100">
+                                        <div class="course">
+                                            <span class="circle"></span>
+                                            <span class="subject"> None of the above </span>
+                                        </div>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="d-flex justify-content-center">
+                            <button class="btn btn-primary px-4 py-2 fw-bold">continue</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- contact area END -->
